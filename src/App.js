317f7b1d4e9191d91d9623685b4eeea32fffa304
Pulkit Pahuja <pulkit.pahuja98@gmail.com>
2021-11-25 00:56:15 +0530

    Basic landing page structure

--- src/App.js
+++ src/App.js
@@ -103,6 +103,7 @@ import { css } from "styled-components/macro"; //eslint-disable-line
 
 import ComponentRenderer from "ComponentRenderer.js";
 import MainLandingPage from "MainLandingPage.js";
+import LandingPage from "pages/LandingPage/LandingPage";
 import ThankYouPage from "ThankYouPage.js";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -110,7 +111,6 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 export default function App() {
   // If you want to disable the animation just use the disabled `prop` like below on your page's component
   // return <AnimationRevealPage disabled>xxxxxxxxxx</AnimationRevealPage>;
-
 
   return (
     <Router>
@@ -123,6 +123,9 @@ export default function App() {
         </Route>
         <Route path="/thank-you">
           <ThankYouPage />
+        </Route>
+        <Route path="/test">
+          <LandingPage />
         </Route>
         <Route path="/">
           <MainLandingPage />
